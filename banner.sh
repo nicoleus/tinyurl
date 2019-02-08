18bb2113c16516bb76aa3e347c6a43808b984247
widhisec <39062772+widhisec@users.noreply.github.com>
2019-02-08 15:48:12 +0700

    Add files via upload

---
+++ banner.sh
@@ -0,0 +1,15 @@
+# widhisec
+BANNER() {
+cat << "asu"
+        .--.
+       |o_o |
+       |:_/ |
+      //   \ \
+     (|     | )
+    /'\_   _/`\
+    \___)=(___/
+(CREATE BY WIDHISEC)
+(SHORTLINK GENERATOR)
+asu
+}
+BANNER
---
+++ tinyurl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# JANGAN DI RECODE TOLONG HARGAI PEMBUAT NYA!
+bash banner.sh
+DATA() {
+z="
+";nz='eval';jz='")'\'';';wz='$Kz$';hz='te'\'';';iz='Oz='\''';Oz='Ez='\''';Tz='Nz='\''';Kz='$(cu';Yz='Jz='\''';kz='Az='\''';Fz='rl -';uz='Hz$I';lz='URL=';ABz='$Oz"';Mz='='\''ny';tz='$Gz$';sz='z$Fz';mz=''\'';';Pz='ttp:';Ez='Cz='\''';Xz='ap'\'';';Zz='i-cr';Wz='='\''m/';Rz='='\''.p';gz='='\''ea';Jz='Bz='\''';xz='Lz$M';pz='z$Bz';Vz=''\'';Iz';Az='z="';Qz=''\'';Lz';vz='z$Jz';Dz='rl'\'';';Bz='";Mz';fz=''\'';Kz';Hz='='\''//';Sz='hp'\'';';Nz='ur'\'';';oz=' "$A';rz='Dz$E';yz='z$Nz';Uz='=$lo';Lz=''\'';Gz';cz='co'\'';';bz='='\''l.';dz='Dz='\''';Cz='='\''?u';Gz=''\'';Fz';az=''\'';Hz';ez='s "h';qz='$Cz$';Iz='ti'\'';';
+eval "$Az$z$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$z$nz$oz$pz$qz$rz$sz$tz$uz$vz$wz$xz$yz$ABz"
+printf "LINK ASLI => $lo"
+printf "RESULT => $URL\n"
+}
+read -p "Masukkan Link => " lo
+DATA $lo
